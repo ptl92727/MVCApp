18e682833025382ef418bfc465c193ebb5b47805
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2023-10-12 12:40:44 +0100

    Add files via upload

---
+++ webApp_mvc/webApp_mvc/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApp_mvc.Models;
+
+namespace webApp_mvc.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions <AppDbContext> options): base (options)
+        {
+            
+        }
+        public DbSet <User>
+
+    }
+}
---
+++ webApp_mvc/webApp_mvc/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations; //data annotation related to data
+
+namespace webApp_mvc.Models
+{
+    public class User
+    {
+
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public DateTime DOB { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
