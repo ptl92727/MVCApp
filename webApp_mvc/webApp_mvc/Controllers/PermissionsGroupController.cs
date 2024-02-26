a3befd3706567ff621cef0aa1e3180adc8c29956
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-26 15:32:57 +0000

    Update PermissionsGroupController.cs

--- webApp_mvc/webApp_mvc/Controllers/PermissionsGroupController.cs
+++ webApp_mvc/webApp_mvc/Controllers/PermissionsGroupController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace TMS.Controllers
+namespace webApp_mvc.Controllers
 {
     public class PermissionsGroupController : Controller
     {
