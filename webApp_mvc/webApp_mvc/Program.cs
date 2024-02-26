9186e879b6f0d015f336e55ec3a0105c9f5c8e13
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-26 16:13:38 +0000

    Update Program.cs

--- webApp_mvc/webApp_mvc/Program.cs
+++ webApp_mvc/webApp_mvc/Program.cs
@@ -5,8 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddIdentity<User, Role>(opt =>
+{
+    opt.Password.RequiredLength = 16;
+    opt.Password.RequireNonAlphanumeric = true;
+    opt.Password.RequireDigit = true;
+    opt.Password.RequireLowercase = true;
+    opt.Password.RequireUppercase = true;
+    opt.User.RequireUniqueEmail = true;
+}
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
