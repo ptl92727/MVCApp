496afae7de5591e69ee8275bc3c3ecef00167331
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-27 16:08:04 +0000

    Update KnowledgeType.cs

--- webApp_mvc/webApp_mvc/Data/KnowledgeType.cs
+++ webApp_mvc/webApp_mvc/Data/KnowledgeType.cs
@@ -1,4 +1,10 @@
 namespace webApp_mvc
 {
     public class KnowledgeLevel
+    {
+
+        public int KnowledgeLevel_ID { get; set; }
+        public string KnowledgeLevel { get; set; }
+        public
+    }
 }
