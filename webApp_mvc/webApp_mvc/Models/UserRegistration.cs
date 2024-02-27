86bce03d16ee208b4d2cea7cae334344c3f5cba6
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-27 11:50:41 +0000

    Update and rename UserRegistration to UserRegistration.cs

---
+++ webApp_mvc/webApp_mvc/Models/UserRegistration.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.EntityF;
+using System.ComponentModel.DataAnnotations;
+
+namespace webApp_mvc
+{
+  public class UserRegisterationModel
+  {
+
+    public string First_Name { get; set; }
+    public string Last_Name { get; set; }
+
+    public UserType Type { get; set; }
+
+
+    [Required(ErrorMessage = "Email Is Required")]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required(ErrorMessage = "Password Is Required")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Compare(nameof(Password),ErrorMessage = "Password Isn't Correct")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set;}
+  }
+}
