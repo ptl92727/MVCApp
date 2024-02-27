48d9e92f0f15c5e364dbcb82f9ab5639e6cef09e
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-27 10:24:42 +0000

    Update User.cs

--- webApp_mvc/webApp_mvc/Data/User.cs
+++ webApp_mvc/webApp_mvc/Data/User.cs
@@ -1,2 +1,18 @@
 using Microsoft.AspNetCore.Identity
-namespace webApp_mvc
+namespace webApp_mvc.Data
+{
+  public class User:IdentityUser<int>
+  {
+    // public int User_ID { get; set; }
+    public string User_Name { get; set; }
+
+    public UserType Type { get; set; }
+    public int User_ID { get; set; }
+  }
+  public enum UserType
+  {
+    Staff,
+    manager
+    
+  }
+}
