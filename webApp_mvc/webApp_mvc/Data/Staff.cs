f7869837c4f00a007d7f838a8e77940d07ec22cc
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-27 10:32:10 +0000

    Create Staff.cs

---
+++ webApp_mvc/webApp_mvc/Data/Staff.cs
@@ -0,0 +1,9 @@
+namespace webApp_mvc.Data
+{
+  public class Staff
+  {
+    public int Staff_ID { get; set; }
+    public String Staff_Name { get; set;}
+    public String Email { get; set}
+  }
+}
