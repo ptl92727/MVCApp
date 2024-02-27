2740830e1e837b366774e787024dc6f1cc137f77
Jacob Law <137056742+ptl92727@users.noreply.github.com>
2024-02-27 14:53:33 +0000

    Update Knowledge.cs

--- webApp_mvc/webApp_mvc/Data/Knowledge.cs
+++ webApp_mvc/webApp_mvc/Data/Knowledge.cs
@@ -2,7 +2,10 @@ namespace webApp_mvc.Data
 {
   public class Knowledge
   {
-    public int Knowledge_Id { get; set; }
-    public string Knowledge_Type { get; set; }
+      public int Knowledge_Id { get; set; }
+      public string Knowledge_Type { get; set; }
+      public string Description { get; set;}
+
+    public List<KnowledgeType> Knowledge { get; set; } = new();
   }
 }
